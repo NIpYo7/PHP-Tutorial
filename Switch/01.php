7dd1ed0d68295b6f8fb527196c242cfb1c5ffe78
NIpYo7 <nipunsuranimala40@gmail.com>
2025-08-27 01:24:09 +0530

    Switch Statement
    Learning switch statement

---
+++ Switch/01.php
@@ -0,0 +1,26 @@
+<?php
+
+$favcolor="red";
+
+switch($favcolor){
+
+    case "Red":
+        echo "Your favorite color is red";
+        break;
+    case "Blue":
+        echo "Your favorite color is blue";
+        break;
+    case "Green":
+        echo "Your favorite color is green";    
+        break;
+    default:
+        echo "Your favorite color is neither red, blue, nor green";        
+        break;  
+}
+
+
+
+
+
+
+?>
---
+++ Switch/02.php
@@ -0,0 +1,27 @@
+<?php 
+
+$d=3;
+swiotch($d){
+    case 1:
+    case 2:
+    case 3: 
+    case 4:
+    case 5:
+        echo "The weeks Feels so long";
+        break;
+    case 6:
+    case 0;
+        echo "Weekends are the best";
+        break;
+    default;
+        echo "Somthing went werong";
+        break;
+
+
+
+}
+
+
+
+
+?>
