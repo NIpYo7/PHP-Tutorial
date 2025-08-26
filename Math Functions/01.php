a16387392db422a12b9be34af0c8cb2f044ee42e
NIpYo7 <nipunsuranimala40@gmail.com>
2025-08-26 22:55:22 +0530

    Math functions
    Learning Math Functions

---
+++ Math Functions/01.php
@@ -0,0 +1,40 @@
+<?php
+
+echo pi(); //3.1415926535898
+
+echo "<br>";
+
+echo min(0,150,30,-8,-200); //-200
+
+echo "<br>";
+
+echo max(0,150,30,-8,-200); //150
+
+echo "<br>";
+
+echo abs(-6.7); //6.7
+
+echo "<br>";
+
+echo sqrt(64); //8
+
+echo "<br>";
+
+echo round(0.60); //1
+echo "<br>";
+echo round(0.49); //0
+echo "<br>";
+
+
+echo rand(); //random number
+echo "<br>";    
+
+echo rand(10,100); //random number between 10 and 100
+
+
+
+
+
+
+
+?>
